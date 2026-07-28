1be475cb29a602f44c30097dbb26d7178c97c25b
Sakshi Agrawal <sakshiag.83@gmail.com>
2026-07-28 22:45:02 +0545

    blog creation API

--- index.js
+++ index.js
@@ -1,39 +1,53 @@
 const express = require("express");
 
 const app = express();
-
 const PORT = 3000;
 
 // Middleware
 app.use(express.json());
+
+// Store blog posts
+let blogs = [];
 
 // Home Route
 app.get("/", (req, res) => {
     res.send("Welcome to the Blog Management System API!");
 });
 
-// GET Route
+// View all blogs
 app.get("/blogs", (req, res) => {
-
-    res.json({
-        message: "GET request received successfully!",
-        blogs: []
-    });
-
+    res.json(blogs);
 });
 
-// POST Route
+// Add a new blog
 app.post("/blogs", (req, res) => {
 
-    const blog = req.body;
+    const { title, author, category, content } = req.body;
+
+    if (!title || !author || !category || !content) {
+        return res.status(400).json({
+            message: "Please fill in all fields."
+        });
+    }
+
+    const newBlog = {
+        id: blogs.length + 1,
+        title,
+        author,
+        category,
+        content
+    };
+
+    blogs.push(newBlog);
 
     res.status(201).json({
-        message: "Blog received successfully!",
-        data: blog
+        message: "Blog added successfully!",
+        blog: newBlog
     });
 
 });
 
+// Start server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });

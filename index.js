d5de8698a3fa95d0dbcdc4b157e91237aa2a8160
Sakshi Agrawal <sakshiag.83@gmail.com>
2026-07-28 22:38:45 +0545

    Express Server

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require("express");
 
 const app = express();
+
 const PORT = 3000;
 
+// Middleware
+app.use(express.json());
+
+// Home Route
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Welcome to the Blog Management System API!");
+});
+
+// GET Route
+app.get("/blogs", (req, res) => {
+
+    res.json({
+        message: "GET request received successfully!",
+        blogs: []
+    });
+
+});
+
+// POST Route
+app.post("/blogs", (req, res) => {
+
+    const blog = req.body;
+
+    res.status(201).json({
+        message: "Blog received successfully!",
+        data: blog
+    });
+
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });

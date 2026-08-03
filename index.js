0ce3e27f312debed71ae29e0b86e3a0118f41b04
Sakshi Agrawal <sakshiag.83@gmail.com>
2026-08-03 19:03:13 +0545

    final

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const express = require("express");
 const Blog = require("./models/Blog");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(__dirname));
